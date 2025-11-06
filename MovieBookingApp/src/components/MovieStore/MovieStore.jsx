0523e9f81a5e4d0f4b980ecbca3aea80bc943a79
Mirza766 <saifmirza766@gmail.com.in>
2025-11-07 03:27:02 +0500

    Styling of a Movie PageStore

--- MovieBookingApp/src/components/MovieStore/MovieStore.jsx
+++ MovieBookingApp/src/components/MovieStore/MovieStore.jsx
@@ -4,7 +4,7 @@ import { useDispatch,useSelector } from 'react-redux'
 import { productList } from '../../redux/Product/ProductActions'
 import { addDataToCart,removeDataFromCart,deleteDataFromCart } from '../../redux/CartData/CartDataActions'
 import Header from './Header'
-
+import "../stylingSheets/MovieStore.css";
 function MovieStore() {
 
 
@@ -20,29 +20,29 @@ const MovieData=useSelector((state)=>state.getMovieData);
 console.log(MovieData);
 
   return (
-    <div>
 
-   
   <div className='movieproduct-container'>
-    <h2>Movie Box</h2>
-    
+    <h2 className='movieproduct-head'>Buy Your Favourate Movie</h2>
+    <div className='movieproductData'>
     {MovieData?.map((movie)=>(
       <div className='movieproductMasterCont' key={movie.id}>
-
+      <img className='movieproduct-img' src={movie.poster_url}/>
+      <div className='content-mastermovie'>
       <h3>{movie.title}</h3>
-      <p><b>Movie: </b>{movie.release_year}</p>
-      <p><b>Price: </b>{movie.price}</p>
-      <div>
-      <button onClick={()=>dispatch(addDataToCart(movie))}>Add to Cart</button>
-      <button onClick={()=>dispatch(deleteDataFromCart(movie))}>Remove from Cart</button>
+      <p className='movieproduct-para'><b>Movie: </b>{movie.release_year}</p>
+      <p className='movieproduct-para'><b>Price: </b>{movie.price}</p>
+      </div>
+      <div className='movieproduct-btns'>
+      <button className='movieproduct-btns-inner' onClick={()=>dispatch(addDataToCart(movie))}>Add to Cart</button>
+      <button className='movieproduct-btns-inner' onClick={()=>dispatch(deleteDataFromCart(movie))}>Remove from Cart</button>
       </div>
       </div>
      
     ))}
-   <button onClick={()=>dispatch(removeDataFromCart())}>Empty Cart</button>
+    </div>
+   <button className='movieproduct-btns-inner' onClick={()=>dispatch(removeDataFromCart())}>Empty Cart</button>
   </div>
-  <Header/>
-     </div>
+
   )
 }
 
--- MovieBookingApp/src/redux/Product/ProductSaga.js
+++ MovieBookingApp/src/redux/Product/ProductSaga.js
@@ -2,7 +2,7 @@ import {takeLatest,call,put} from 'redux-saga/effects'
 import { GET_PRODUCT_DATA,SET_PRODUCT_DATA } from './ProductConstants'
 
 function *getProducts(){
-const response=yield call(fetch,"http://localhost:3000/movie")
+const response=yield call(fetch,"http://localhost:4000/movie")
 const payload=yield response.json();
 console.warn('All Products', payload);
 yield put({type:SET_PRODUCT_DATA,payload});
